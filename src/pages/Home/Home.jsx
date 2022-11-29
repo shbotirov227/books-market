4af7dce4f862ac4813b488369b46b6a1bd42133b
Shohruh174 <shbotirov227@gmail.com>
2022-11-29 21:43:00 +0500

    page

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -14,6 +14,7 @@ import { Keyboard } from "swiper";
 import BtnCategory from "../../components/BtnCategory/BtnCategory";
 import Nums from "../../components/Nums/Nums";
 
+
 const Home = () => {
     return (
         <div className="Home">
